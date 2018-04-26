f7bd0f2280f7114dd91fb77fc76ee638a989d264
Derelanin <solo.akicare@gmail.com>
2018-04-26 20:27:18 +0300

    help

--- ping/my_help.cpp
+++ ping/my_help.cpp
@@ -3,10 +3,10 @@
 
 
 void my_help(void) {
-	cout << TEXT("Утилита ping") << endl;
-	cout << TEXT("Требования к системе: OS Windows") << endl;
+	cout << TEXT("Ping — утилита для проверки целостности и качества соединений в сетях на основе TCP/IP.") << endl;
+	cout << TEXT("Требования к системе: OS Windows") << endl << endl;
 	cout << TEXT("Список аргументов:") << endl;
-	cout << TEXT("1 параметр: IP/DNS. \tDefault: help") << endl;
-	cout << TEXT("2 параметр: файл лога. \tDefault: лог не ведётся") << endl;
-	//system("pause");
+	cout << TEXT("1 параметр: IP/DNS. \t\tDefault: help") << endl;
+	cout << TEXT("2 параметр: путь к файлу лога. \tDefault: лог не ведётся") << endl << endl;
+	cout << TEXT("Авторы программы: Иванникова Валентина, Шариков Алексей") << endl;
 }
